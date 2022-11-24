daa78c7421b7c886b3901530d8bebbb9f9be630c
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2022-11-24 14:05:35 +0700

    Create Simple Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+
 public class Main {
+    public Main() {
+        JFrame frame = new JFrame();
+        frame.setTitle("CPTA - Crasy pasta of things assembling");
+        frame.setSize(1280, 720 + 25);
+        frame.setLocation(0, 0);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main();
     }
 }
